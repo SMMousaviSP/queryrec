4755453eb83891f51a27ecbc8fba0e95f98d0c00
omar facchini <omar.facchini@studenti.unitn.it>
2023-01-05 16:09:27 +0100

    created function to generate utility matrix, a bit basic and has to be expanded based on the size of the users we want to achieve, can be added other combinations of params for the bias in order to have some sense instead of random

--- src/datagen/dataGenerator.py
+++ src/datagen/dataGenerator.py
@@ -4,7 +4,7 @@ import random
 from faker import Faker
 
 # user entries is the number of users we want in the db
-USER_ENTRIES = 10
+USER_ENTRIES = 50
 
 # entries of each parameter, like 10 different cities, 10 different ages..
 TOTAL_ENTRIES = 10
@@ -125,15 +125,57 @@ def generate_query_set(ids, names, surnames, heights, ages):
             data.clear()
 
 
+def generate_utility_matrix():
+    with open('src\\dataset\\utilityMatrix.csv', 'w', newline='') as file:
+        with open('src\\dataset\\querySet.csv',  'r', newline='') as myInput:
+            reader = csv.reader(myInput, delimiter=',')
+            writer = csv.writer(file)
+            header = list(range(QUERY_ENTRIES))
+            writer.writerow(header)
+            for i in range(0, USER_ENTRIES):
+                #ids of user set starts from USER_ENTRIES
+                data = ['user'+str(i + USER_ENTRIES)]
+                for row in reader:
+                    if(i < 10): #make first 10 users like queries that have names that start with R
+                        for item in row: #this only works on single params, doesn't work with AND
+                            if(item.split('=')[0] == 'name' and item.split('=')[1].startswith(('R','r'))):
+                                data.append(random.sample(range(70,100),1)[0])
+
+                    elif(i >= 10 and i <20): #make next 10 users dislike queries that have names that start with R
+                        for item in row: #this only works on single params, doesn't work with AND
+                            if(item.split('=')[0] == 'name' and item.split('=')[1].startswith(('R','r'))):
+                                data.append(random.sample(range(0,40),1)[0])
+
+                    elif(i >= 20 and i < 30): #make next 10 users like queries with age AND name starting with R
+                        items = [item for item in row if item.startswith(('name','age'))] # have a list with the params we need
+                        if(len(items) == 2): #check if we got the right amount of params
+                            if(items[0].split('=')[1].startswith(('R','r')) and int(items[1].split('=')[1]) >= 36):
+                                data.append(random.sample(range(70,100),1)[0])
+
+                    #add different camps to include different combinations of params and preferences for the users
+                    #as of now we have 50 users and i have built the matrix for 30 of them, the other 20 are completely empty as if they have never posed a query
+                            
+
+                    data.append('') #append empty element as the query was not posed before
+
+                #print(data)
+
+                writer.writerow(data)
+                data.clear()
+                myInput.seek(0) #reset file pointer to begin
+                #could also store the data in memory and iterate how many times we want instead of doing this, depends on the size, this always works.
+
+
 
 def main():
     ids = generate_ids()
     names, surnames = generate_names_surnames()
     heights = generate_heights()
     ages = generate_age()
-    generate_people(ids, names, surnames, heights, ages)
-    generate_user_set()
-    generate_query_set(ids, names, surnames, heights, ages)
+    #generate_people(ids, names, surnames, heights, ages)
+    #generate_user_set()
+    #generate_query_set(ids, names, surnames, heights, ages)
+    generate_utility_matrix()
 
 
 if __name__ == "__main__":
